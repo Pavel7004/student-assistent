4d246c8c8811a21efe18c72c882ae6bba0e3a875
Kovalev Pavel <kovalev5690@gmail.com>
2024-04-20 22:17:09 +0300

    Return data on `/courses`

--- app.py
+++ app.py
@@ -1,21 +1,45 @@
-from flask import Flask, render_template
+from flask import Flask, Response
 import json
+import psycopg2
 
-port=5000
+port = 5000
 
 app = Flask(__name__)
 
-@app.route("/")
-def index():
-    return render_template("index.html")
+db_config = {
+    'dbname': 'student_courses',
+    'user': 'test',
+    'password': 'test',
+    'host': 'localhost',
+    'port': '5432'
+}
 
-@app.route('/courses', methods=['GET'])
+
+class DBAdapter:
+
+    def __init__(self):
+        self.conn = psycopg2.connect(**db_config)
+
+    def query(self, sql_query):
+        cursor = self.conn.cursor()
+
+        cursor.execute(sql_query)
+
+        cursor.close()
+
+
+db = DBAdapter()
+
+
+@app.route('/courses', methods=['POST'])
 def courses():
-    data = request.get_json()  # Получение данных JSON из запроса
-    return answer
-    
+    # {russian: ${russian}, math: ${math}, social: ${social}, language: ${language}, informat: ${informat},
+    #              biology: ${biology}, geography: ${geography}, chemistry: ${chemistry}, physics: ${physics}, history: ${history},
+    #             literature: ${literature}, isMedal: ${isMedal}, isGto: ${isGto}, isVolunteering: ${isVolunteering}};
+    data = json.loads(Response.form.get('json_data'))
 
+    return data
 
 
 if __name__ == "__main__":
-    app.run(debug=True,port=port)
+    app.run(debug=True, port=port)
