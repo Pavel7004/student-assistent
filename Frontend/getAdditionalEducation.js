3866275a01304af8ff2c9cfeeb52322384b78fb6
Kupidon4a <alexs_petrov_2003@mail.ru>
2024-04-21 15:41:46 +0300

    Frontend 6. Finish
    Завершена разработка клиентской части модуля Дополнительное образование.

---
+++ Frontend/getAdditionalEducation.js
@@ -0,0 +1,19 @@
+console.log(typeof(localStorage["data"]) + JSON.parse(localStorage["data"])[0].name);
+
+const dataContainer = document.getElementById('dataContainer');
+
+responseData = JSON.parse(localStorage["data"]);
+
+function handlerButton(responseData){
+    for (let i = 0; i < responseData.length; i++){
+        console.log(i);
+        const divElement = document.createElement('div');
+		divElement.classList.add('item');
+        let pElement = document.createElement('p');
+        pElement.textContent = `Название курса: ${responseData[i]}`;
+        divElement.appendChild(pElement);
+        dataContainer.appendChild(divElement);
+    }
+}
+
+handlerButton(responseData);
